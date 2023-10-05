dd57aa565ba6714f3987fa90515c3dfd695b03d3
Bhavya KB <bhavyabhavadas21@gmail.com>
2023-10-05 11:55:32 +0530

    background gradient

--- lib/Login/loginpage.dart
+++ lib/Login/loginpage.dart
@@ -10,73 +10,131 @@ class Login extends StatefulWidget {
 }
 
 class _Login extends State<Login> {
+  OutlineInputBorder myinputborder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderSide: BorderSide(color: Colors.white, width: 3),
+    );
+  }
+
+  OutlineInputBorder myfocusborder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderSide: BorderSide(color: Color.fromARGB(255, 216, 125, 7), width: 3),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.7,
-            // decoration: BoxDecoration(
-            //   shape: BoxShape.rectangle,
-            //   color: Colors.blue,
-            // ),
-            child: Column(children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.1,
-                alignment: Alignment.center,
-               
-                child: Image(image: AssetImage('assets/image1.jpg'),),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 1.0,
-                height: MediaQuery.of(context).size.height * 0.1,
-                alignment: Alignment.center,
-                // decoration: BoxDecoration(
-                //   shape: BoxShape.rectangle,
-                //   color: Colors.red,
-                // ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    hintText: 'Enter Mobile Number',
-                  ),
+        // backgroundColor: Color.fromARGB(255, 46, 9, 129),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              // alignment: Alignment.center,
+              //  decoration: BoxDecoration(
+              // gradient: LinearGradient(
+              //     begin: Alignment.topCenter,
+              //     end: Alignment.bottomCenter,
+              //     colors: [
+              //       Color.fromARGB(255, 238, 145, 7),
+              //       Color.fromARGB(255, 142, 131, 240),
+              //       Color.fromARGB(255, 230, 98, 127),
+              //     ]),
+              // shape: BoxShape.circle,
+              // color: Colors.white,
+              // ),
+
+              decoration: new BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [
+                    0.2,
+                    0.2,
+                    // 0.8,
+                  ],
+                  colors: [
+                    Color.fromARGB(255, 216, 125, 7),
+                    Color.fromARGB(255, 14, 13, 17),
+                    // Colors.yellow,
+                  ],
                 ),
               ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.1,
-                alignment: Alignment.center,
-                // decoration: BoxDecoration(
-                //   shape: BoxShape.rectangle,
-                //   color: Colors.purple,
-                // ),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Otp()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 12),
-                  ),
-                  child: Text(
-                    "Next",
-                    style: GoogleFonts.alice(
-                      fontSize: 18,
+
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: Column(children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    alignment: Alignment.center,
+                    child: Image(
+                      image: AssetImage('assets/image1.jpg'),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.rectangle,
+                    //   color: Colors.red,
+                    // ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        // border: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.red),
+                        //   borderRadius: BorderRadius.all(Radius.circular(12)),
+                        // ),
+                        hintText: 'Enter Mobile Number',
+                        hintStyle: GoogleFonts.abhayaLibre(color: Colors.white),
+
+                        labelText: "Mobile Number",
+                        labelStyle: GoogleFonts.aBeeZee(
+                          color: Color.fromARGB(255, 216, 125, 7),
+                        ),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  Container(
+                    alignment: Alignment.center,
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.rectangle,
+                    //   color: Colors.purple,
+                    // ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Otp()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 216, 125, 7),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+                      ),
+                      child: Text(
+                        "Next",
+                        style: GoogleFonts.alice(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
               ),
-            ]),
+            ),
           ),
         ),
       ),
--- lib/Login/otppage.dart
+++ lib/Login/otppage.dart
@@ -9,71 +9,129 @@ class Otp extends StatefulWidget {
 }
 
 class _Otp extends State<Otp> {
+  OutlineInputBorder myinputborder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderSide: BorderSide(color: Colors.white, width: 3),
+    );
+  }
+
+  OutlineInputBorder myfocusborder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderSide: BorderSide(color: Color.fromARGB(255, 216, 125, 7), width: 3),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: GestureDetector(
-            onTap: () => {Navigator.pop(context)},
-            child: Icon(
-              Icons.arrow_back,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-        body: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.8,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.blue,
-            ),
-            child: Column(children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-              Container(
-                width: MediaQuery.of(context).size.width * 1.0,
-                height: MediaQuery.of(context).size.height * 0.1,
-                alignment: Alignment.center,
-                // decoration: BoxDecoration(
-                //   shape: BoxShape.rectangle,
-                //   color: Colors.red,
-                // ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    hintText: 'Enter OTP',
-                  ),
+        // appBar: AppBar(
+        //   backgroundColor: Color.fromARGB(255, 216, 125, 7),
+        //   leading: GestureDetector(
+        //     onTap: () => {Navigator.pop(context)},
+        //     child: Icon(
+        //       Icons.arrow_back,
+        //       color: Colors.grey,
+        //     ),
+        //   ),
+        // ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [
+                    0.2,
+                    0.2,
+                    // 0.8,
+                  ],
+                  colors: [
+                    Color.fromARGB(255, 216, 125, 7),
+                    Color.fromARGB(255, 14, 13, 17),
+                    // Colors.yellow,
+                  ],
                 ),
               ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.1,
-                alignment: Alignment.center,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.8,
                 // decoration: BoxDecoration(
                 //   shape: BoxShape.rectangle,
-                //   color: Colors.purple,
+                //   color: Colors.blue,
                 // ),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    padding: EdgeInsets.symmetric(horizontal: 37, vertical: 14),
-                  ),
-                  child: Text(
-                    "Get Started ",
-                    style: GoogleFonts.actor(
-                      fontSize: 11,
+                child: Column(children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    alignment: Alignment.center,
+                    child: Image(
+                      image: AssetImage('assets/image1.jpg'),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 1.0,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    alignment: Alignment.center,
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.rectangle,
+                    //   color: Colors.red,
+                    // ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        // border: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.red),
+                        //   borderRadius: BorderRadius.all(Radius.circular(12)),
+                        // ),
+
+                        hintText: 'Enter OTP',
+                        hintStyle: GoogleFonts.abhayaLibre(color: Colors.white),
+                        labelText: "OTP",
+                        labelStyle: GoogleFonts.aBeeZee(
+                          color: Color.fromARGB(255, 216, 125, 7),
+                        ),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  Container(
+                    // width: MediaQuery.of(context).size.width * 0.3,
+                    // height: MediaQuery.of(context).size.height * 0.1,
+                    alignment: Alignment.center,
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.rectangle,
+                    //   color: Colors.purple,
+                    // ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 216, 125, 7),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 37, vertical: 14),
+                      ),
+                      child: Text(
+                        "Get Started ",
+                        style: GoogleFonts.actor(
+                          fontSize: 11,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
               ),
-            ]),
+            ),
           ),
         ),
       ),
